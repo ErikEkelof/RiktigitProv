a1533bded994519ef25491fb9c6f712c40ee9610
ErikEkelof <60339719+ErikEkelof@users.noreply.github.com>
2020-10-22 16:15:10 +0200

    tjena

---
+++ prov/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Printinfo()
+
+             Evaluate()
+        
+            public class Book {
+                Random generator = new Random();
+                private int  price; 
+                private string name;
+                private int rarity; 
+                private string category; 
+                private int actualValue; 
+                private bool cursed; 
+
+            
+                public Book() {
+                    actualValue = generator.Next(0,501);
+                    rarity = generator.Next(0,11);
+                    cursed = generator.Next(0, 2) > 2; 
+                }
+
+                 public void Printinfo() 
+                {
+                    Console.WriteLine("Name: " + name);
+                    Console.WriteLine("Rarity " + rarity);
+                    Console.WriteLine("Price " + price);
+                }
+
+                public int Evaluate()
+                {
+                   actualValue * rarity == actualValue
+                   
+
+                }
+
+                public bool IsCursed() 
+                {
+                    if (book == IsCursed) 
+                    {
+                        
+                    }
+                }
+
+
+            }
+
+            public class customer 
+            {
+                private int money;
+                private bool interested;
+                private bool IfCursed; 
+                private 
+
+
+            }
+               
+        }
+    }
+}
+
+
